a610988932dc8377f242903c1190738f1e7e548f
Sean Ge <seange727@gmail.com>
2013-04-10 14:45:20 -0400

    diff file

--- upload.py
+++ upload.py
@@ -1,22 +1,21 @@
 import git
 import urllib2
 import filecmp
-import os
-u = urllib2.urlopen('http://www.ncaa.com/newsrss/')
+import os, time
+while True:
+    u = urllib2.urlopen('http://www.ncaa.com/newsrss/')
+    
+    localFile = open('ncaa_New.xml', 'w')
+    
+    localFile.write(u.read())
 
-localFile = open('ncaa_New.xml', 'w')
-#oldFile = open('ncaa.xml', 'r')
-#print localFile==oldFile
-#print filecmp.cmp(localFile, oldFile)
-#if localFile != u.read():
+    localFile.close()
+    
+    if not filecmp.cmp('ncaa.xml', 'ncaa_New.xml'):
+        print "diff file, rename..."
+        os.rename('ncaa_New.xml', 'ncaa.xml')
+        print "finish"
 
-localFile.write(u.read())
-localFile.close()
-if filecmp.cmp('ncaa.xml', 'ncaa_New.xml'):
-    print "diff file, rename..."
-    os.raname('ncaa_New.xml', 'ncaa.xml')
-    print "finish"
-#os.rename(localFile, 'ncaa.xml')
-'''else:
-    print "same file"
-'''
+    else:
+        print "they are same"
+        time.sleep(5)
