94dc4cd952db11fd40d8a3fd568e56e9ae46d3de
Sean Ge <seange727@gmail.com>
2013-04-10 22:44:39 -0400

    another commit

--- upload.py
+++ upload.py
@@ -1,21 +1,41 @@
-import git
+from git import *
 import urllib2
 import filecmp
 import os, time
-while True:
-    u = urllib2.urlopen('http://www.ncaa.com/newsrss/')
-    
-    localFile = open('ncaa_New.xml', 'w')
-    
-    localFile.write(u.read())
+from progressbar import *
+fname = 'ncaa'
+postfix = '.xml'
+dir = os.path.dirname(os.path.abspath(__file__))
+repo = Repo(dir)
+index = repo.index
 
-    localFile.close()
+if repo.is_dirty():
+    print "directory is dirty"
+    repo.git.add('.')
+    commit = index.commit("another commit")
+    print repo.untracked_files
+try:
+    while True:
+        u = urllib2.urlopen('http://www.ncaa.com/newsrss/')
     
-    if not filecmp.cmp('ncaa.xml', 'ncaa_New.xml'):
-        print "diff file, rename..."
-        os.rename('ncaa_New.xml', 'ncaa.xml')
-        print "finish"
+        localFile = open(fname+'_New'+postfix, 'w')
+        
+        localFile.write(u.read())
+        
+        localFile.close()
+        
+        if not filecmp.cmp('ncaa.xml', 'ncaa_New.xml'):
+            print "diff file, rename..."
+            os.rename('ncaa_New.xml', 'ncaa.xml')
+            print "finish"
+            
 
-    else:
-        print "they are same"
-        time.sleep(5)
+        else:
+            print "wait for 5 seconds"
+            pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval = 500).start()
+            for i in range(500):
+                time.sleep(0.01)
+                pbar.update(i+1)
+            pbar.finish()
+except KeyboardInterrupt:
+    print "finish uploading"
